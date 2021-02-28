0a2edb81cd8e7122a0647d41f68ddc05aa97b044
Kunal Tiwary <kunal.tiwary@quantium.com.au>
2021-02-28 20:09:53 +1100

    Comments

--- src/main/scala/au/com/cba/json/models/FoodItem.scala
+++ src/main/scala/au/com/cba/json/models/FoodItem.scala
@@ -10,5 +10,5 @@ case class Candy(price: Double,
 
 //Additional food item type added to show building of newer data types
 case class Fruit(price: Double,
-                 nutrients: Seq[String]
+                 nutrients: Seq[String] //kept as a String type for simplicity. Ideally should be Enums for different nutrients
                 ) extends FoodItem
